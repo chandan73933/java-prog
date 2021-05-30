267467c84d640c90b0cd1e1f1d700ae34b7bb0b5
Chandan Kumar <47857139+chandan73933@users.noreply.github.com>
2021-05-30 16:16:35 +0530

    Add files via upload

---
+++ interface.java
@@ -0,0 +1,44 @@
+//class result extends student implements sport
+class student
+{
+	int x,y;
+	void getdata(int a,int b)
+	{
+		x=a;
+		y=b;
+	}
+	void putdata()
+	{
+		System.out.println("");
+		Sustem.out.println("");
+	}
+}
+interface sports
+{
+	int sp=6;
+	void spmarks();
+
+}
+class result extends student implements sports
+{
+	public void spmarks()
+	{
+		System.out.println("sports marks "+sp);
+	}
+	void disp()
+	{
+		putdata();
+		spmarks();
+		int total = x+y+sp;
+		System.out.println("total"+total);
+	}
+}
+class inter
+{
+	public static void main(String[] args) 
+	{
+		result obj=new result();
+		obj.getdata(23,56);
+		obj.disp();
+	}
+}
