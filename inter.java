b9d20b181db5fe36b567c3c407bb7fb533fa8d1c
Chandan Kumar <47857139+chandan73933@users.noreply.github.com>
2021-05-30 16:14:34 +0530

    Add files via upload

--- inter.java
+++ inter.java
@@ -1,57 +1,44 @@
-class A
+//class result extends student implements sport
+class student
 {
-	int a;
-	void getdata(int x)
+	int x,y;
+	void getdata(int a,int b)
 	{
-		a=x;
+		x=a;
+		y=b;
 	}
-	void  putno()
+	void putdata()
 	{
-		System.out.println("the value of a is "+a);
+		System.out.println("");
+		System.out.println("");
 	}
-}
-class B extends A
-{
-	float c,d;
-	void getmarks(float y,float z)
-	{
-		c=y;
-		d=z;
-	}
-	void putmr()
-	{
-		System.out.println("the value of c is "+c);
-		System.out.println("the value of d is "+d);
-	}
-
 }
 interface sports
 {
-	float sportwt=6.0f;
-	void putwt();
+	int sp=6;
+	void spmarks();
+
 }
-class C extends B implements sports
+class result extends student implements sports
 {
-	float total;
-	public void putwt()
+	public void spmarks()
 	{
-		System.out.println("sports wt="+sportwt);
+		System.out.println("sports marks "+sp);
 	}
-	void display()
+	void disp()
 	{
-		total=c+d+sportwt;
-		putno();
-		putmr();
-		putwt();
-		System.out.println("total score="+total);
+		putdata();
+		spmarks();
+		int total = x+y+sp;
+		System.out.println("total"+total);
 	}
 }
 class inter
 {
-	 public static void main(String[] args) {
-		C obj=new C();
-		obj.getmarks(3,5);
-		obj.getdata(4);
-		obj.display();
+	public static void main(String[] args) 
+	{
+		result obj=new result();
+		obj.getdata(23,56);
+		obj.disp();
 	}
 }
